cc53b38ab41116bb1410e66ec339b2f1ac12a430
Sebastian Pieczyński <testament777@gmail.com>
2021-10-24 21:18:09 +0200

    Make ShoeCards flexible

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -5,14 +5,21 @@ import SHOES from '../../data';
 import ShoeCard from '../ShoeCard';
 
 const ShoeGrid = () => {
-  return (
-    <Wrapper>
-      {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
-      ))}
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			{SHOES.map((shoe) => (
+				<ShoeWrapper>
+					<ShoeCard key={shoe.slug} {...shoe} />
+				</ShoeWrapper>
+			))}
+		</Wrapper>
+	);
 };
+
+const ShoeWrapper = styled.div`
+	min-width: 275px;
+	flex: 1;
+`;
 
 const Wrapper = styled.div`
 	display: flex;
